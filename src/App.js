e12a3eaf4954c48f51ef08bf285ea6a59d299c8d
kimjiwon506 <kimjiwon506@gmail.com>
2022-07-29 10:33:35 +0900

    feat: 컴포넌트 생성 및 스타일추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 // TODO: TodoTemplate, TodoInsert, TodoListItem, TodoList 네개의 컴포넌트로 분리해서 만들기
 function App() {
   return (
     <div className="App"> 
-      테스트
+     <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+     </TodoTemplate>
     </div>
   );
 }
---
+++ src/components/TodoInsert.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MdAdd } from 'react-icons/md';
+import todoInsert from '../style/TodoInsert.scss';
+
+const TodoInsert = () => {
+  return (
+    <form action="" className="TodoInsert">
+      <input type="text" placeholder="할일을 입력하세요." />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
